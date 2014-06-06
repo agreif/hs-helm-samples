558b52501c597b53649541f111b7940590e54835
Alex Greif <alex@greifdesign.net>
2014-06-06 23:20:11 +0200

    added random_sensor sample

---
+++ random_sensor/Main.hs
@@ -0,0 +1,17 @@
+import FRP.Helm
+import qualified FRP.Helm.Window as Window
+
+data State = State {mx :: Double, my :: Double}
+
+step :: [Int] -> State -> State
+step [dx, dy] state = state {mx = mx state + realToFrac dx,
+                             my = my state + realToFrac dy}
+
+render :: State -> (Int, Int) -> Element
+render state (w, h) = centeredCollage w h [move (mx state, my state) $ filled red $ square 64]
+
+main :: IO ()
+main = do
+  run defaultConfig $ render <~ stepper ~~ Window.dimensions
+  where state = State {mx = 0, my = 0}
+        stepper = foldp step state $ combine [randomR (-1, 1), randomR (-1, 1)]
---
+++ random_sensor/README.md
@@ -0,0 +1,4 @@
+random_sensor
+===============
+
+Lets a red square move randomly around.
