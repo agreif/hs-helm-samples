fdc17e47c76e7efbd177347a947c8f29c699dd4f
Alex Greif <alex@greifdesign.net>
2014-06-07 14:27:06 +0200

    refactored code

--- random_sensor/Main.hs
+++ random_sensor/Main.hs
@@ -1,24 +1,25 @@
 import FRP.Helm
 import qualified FRP.Helm.Window as Window
 
-data State = State {mx :: Double, my :: Double, speed :: Double}
+data State = State {mx :: Double, my :: Double, speed :: Double, color :: Color}
 
 step :: [Int] -> State -> State
 step [dx, dy] state = state {mx = mx state + realToFrac dx * speed state,
                              my = my state + realToFrac dy * speed state}
 
-render :: State -> State -> State -> (Int, Int) -> Element
-render state1 state2 state3 (w, h) = centeredCollage w h [move (mx state1, my state1) $ filled red $ square 64,
-                                                          move (mx state2, my state2) $ filled blue $ square 64,
-                                                          move (mx state2, my state3) $ filled yellow $ square 64]
+render :: [State] -> (Int, Int) -> Element
+render states (w, h) = centeredCollage w h forms
+  where forms = map (\state -> createForm state) states
+        createForm :: State -> Form
+        createForm state = move (mx state, my state) $ filled (color state) $ square 64
 
 main :: IO ()
 main = do
-  run config $ render <~ (stepper state1) ~~ (stepper state2) ~~ (stepper state3) ~~ Window.dimensions
+  run config $ render <~ squares ~~ Window.dimensions
   where config = defaultConfig {windowTitle = "random_sensor",
                                 windowDimensions = (500, 500)}
-        state1 = State {mx = 0, my = -30, speed = 1}
-        state2 = State {mx = 0, my = 30, speed = 2}
-        state3 = State {mx = 0, my = 30, speed = 5}
+        state1 = State {mx = 0, my = -30, speed = 1, color = red}
+        state2 = State {mx = 0, my = 30, speed = 2, color = blue}
+        state3 = State {mx = 0, my = 30, speed = 5, color = yellow}
         stepper state = foldp step state $ combine [randomR (-1, 1), randomR (-1, 1)]
-
+        squares = combine [(stepper state1), (stepper state2), (stepper state3)]
