5ae189420cf9016a3fb5ad2009a542020b17304f
Alex Greif <alex@greifdesign.net>
2014-06-08 23:04:21 +0200

    moving invader

--- space_invaders/Main.hs
+++ space_invaders/Main.hs
@@ -6,6 +6,10 @@ import FRP.Elerea.Simple
 data CannonState = CannonState {cx :: Double, cy :: Double,
                                 lx :: Double, ly :: Double, laserFlying :: Bool}
 
+data EnemyDirection = ToLeft | ToRight
+data InvaderState = InvaderState {ix :: Double, iy :: Double, direction :: EnemyDirection, stepsX :: Int}
+
+
 cannonSignal :: Int -> SignalGen(Signal CannonState)
 cannonSignal winHeight = signal
   where initialState = CannonState {cx = 0, cy = realToFrac winHeight / 2 * 0.8,
@@ -22,21 +26,48 @@ cannonSignal winHeight = signal
                     | otherwise = cx'
                 ly' = if laserFlying' then ly state + 1 else 0
 
+invaderSignal :: SignalGen(Signal InvaderState)
+invaderSignal = signal
+  where initialState = InvaderState {ix = -100, iy = -100, direction = ToRight, stepsX = -2}
+        signal = foldp newState initialState count
+        newState :: Int -> InvaderState -> InvaderState
+        newState sampleCount state = state {ix = ix', iy = iy',
+                                            stepsX = stepsX'}
+          where ix' = ix state + dx
+                iy' = iy state + dy
+                sleep = 150
+                stepNow = sampleCount `mod` (sleep) == 0
+                steps = 6
+                (dhori, dverti) = (20, 20)
+                stepsDoublePlus1 = steps * 2 + 1
+                stepsX' | stepNow && stepsX state == stepsDoublePlus1 = 0
+                        | stepNow = stepsX state + 1
+                        | otherwise = stepsX state
+                (dx, dy) | stepNow && (stepsX' == steps || stepsX' == stepsDoublePlus1) = (0, dverti)
+                         | stepNow && stepsX' < steps = (dhori, 0)
+                         | stepNow && stepsX' > steps = (-dhori, 0)
+                         | otherwise = (0, 0)
+
 cannonForm :: CannonState -> Form
-cannonForm state = move (cx state, cy state) $ filled red $ square 64
+cannonForm state = move (cx state, cy state) $ filled red $ rect 64 32
 
 laserForm :: CannonState -> Form
 laserForm cannonState = move (lx', ly') $ filled white $ rect 2 10
   where lx' = lx cannonState
-        ly' = cy cannonState - 30 - ly cannonState
+        ly' = cy cannonState - 15 - ly cannonState
 
-render :: CannonState -> (Int, Int) -> Element
-render cannonState (w, h) = centeredCollage w h [cannonForm cannonState,
-                                                 laserForm cannonState]
+invaderForm :: InvaderState -> Form
+invaderForm invaderState = move (ix invaderState, iy invaderState) $ filled white $ rect 20 20
+
+render :: CannonState -> InvaderState -> (Int, Int) -> Element
+render cannonState invaderState (w, h) =
+  centeredCollage w h $ [cannonForm cannonState,
+                         laserForm cannonState,
+                         invaderForm invaderState]
 
 main :: IO ()
 main = do
-  run config $ render <~ (cannonSignal winHeight) ~~ Window.dimensions
+  run config $ render <~ (cannonSignal winHeight) ~~ invaderSignal ~~ Window.dimensions
   where config = defaultConfig {windowTitle = "space_invaders",
                                 windowDimensions = (winWidth, winHeight)}
         (winWidth, winHeight) = (500, 500)
