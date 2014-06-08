e8568b21b58e7a0be4adb2084a2f10a5a405a5bc
Alex Greif <alex@greifdesign.net>
2014-06-08 20:13:26 +0200

    laser flies correctly

--- space_invaders/Main.hs
+++ space_invaders/Main.hs
@@ -3,40 +3,40 @@ import qualified FRP.Helm.Window as Window
 import qualified FRP.Helm.Keyboard as Keyboard
 import FRP.Elerea.Simple
 
-data CannonState = CannonState {cx :: Double, cy :: Double}
-data LaserState = LaserState {ly :: Double, flying :: Bool}
+data CannonState = CannonState {cx :: Double, cy :: Double,
+                                lx :: Double, ly :: Double, laserFlying :: Bool}
 
 cannonSignal :: Int -> SignalGen(Signal CannonState)
 cannonSignal winHeight = signal
-  where initialState = CannonState {cx = 0, cy = realToFrac winHeight / 2 * 0.8}
-        signal = foldp moveCannon initialState Keyboard.arrows
-        moveCannon :: (Int, Int) -> CannonState -> CannonState
-        moveCannon (dx, _) state = state {cx = cx state + realToFrac dx}
+  where initialState = CannonState {cx = 0, cy = realToFrac winHeight / 2 * 0.8,
+                                    lx = 0, ly = 0, laserFlying = False}
+        signal = foldp newState initialState (lift2 combine' Keyboard.arrows Keyboard.space)
+        combine' :: (Int, Int) -> Bool -> ((Int, Int), Bool)
+        combine' (dx, dy) fired = ((dx, dy), fired)
+        newState :: ((Int, Int), Bool) -> CannonState -> CannonState
+        newState ((dx, _), fired) state = state {cx = cx',
+                                                 lx = lx', ly = ly', laserFlying = laserFlying'}
+          where cx' = cx state + realToFrac dx
+                laserFlying' = (fired && ly state == 0) || (ly state /= 0 && ly state < 300)
+                lx' | laserFlying' = lx state
+                    | otherwise = cx'
+                ly' = if laserFlying' then ly state + 1 else 0
 
 cannonForm :: CannonState -> Form
 cannonForm state = move (cx state, cy state) $ filled red $ square 64
 
-laserSignal :: SignalGen(Signal LaserState)
-laserSignal = signal
-  where initialState = LaserState {ly = 0, flying = False}
-        signal = foldp fireLaser initialState Keyboard.space
-        fireLaser :: Bool -> LaserState -> LaserState
-        fireLaser fired state = state {flying = flying, ly = ly'}
-          where flying = (fired && (ly state == 0)) || (ly state /= 0 && ly state < 300)
-                ly' = if flying then ly state + 1 else 0
+laserForm :: CannonState -> Form
+laserForm cannonState = move (lx', ly') $ filled white $ rect 2 10
+  where lx' = lx cannonState
+        ly' = cy cannonState - 30 - ly cannonState
 
-laserForm :: LaserState -> CannonState -> Form
-laserForm laserState cannonState = move (lx', ly') $ filled white $ rect 2 10
-  where lx' = cx cannonState
-        ly' = cy cannonState - 30 - ly laserState
-
-render :: CannonState -> LaserState -> (Int, Int) -> Element
-render cannonState laserState (w, h) = centeredCollage w h [cannonForm cannonState,
-                                                            laserForm laserState cannonState]
+render :: CannonState -> (Int, Int) -> Element
+render cannonState (w, h) = centeredCollage w h [cannonForm cannonState,
+                                                 laserForm cannonState]
 
 main :: IO ()
 main = do
-  run config $ render <~ (cannonSignal winHeight) ~~ laserSignal ~~ Window.dimensions
+  run config $ render <~ (cannonSignal winHeight) ~~ Window.dimensions
   where config = defaultConfig {windowTitle = "space_invaders",
                                 windowDimensions = (winWidth, winHeight)}
         (winWidth, winHeight) = (500, 500)
