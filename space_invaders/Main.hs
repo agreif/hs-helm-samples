0ebbab50f65132370b5545f33f15011096957183
Alex Greif <alex@greifdesign.net>
2014-06-08 19:33:55 +0200

    fire laser

--- space_invaders/Main.hs
+++ space_invaders/Main.hs
@@ -3,22 +3,40 @@ import qualified FRP.Helm.Window as Window
 import qualified FRP.Helm.Keyboard as Keyboard
 import FRP.Elerea.Simple
 
-data SpaceshipState = SpaceshipState {mx :: Double, my :: Double}
+data CannonState = CannonState {cx :: Double, cy :: Double}
+data LaserState = LaserState {ly :: Double, flying :: Bool}
 
-createSpaceship :: Int -> SignalGen(Signal SpaceshipState)
-createSpaceship winHeight = spaceshipSignal
-  where state = SpaceshipState {mx = 0, my = realToFrac winHeight / 2 * 0.8}
-        spaceshipSignal = foldp moveSpaceship state Keyboard.arrows
+cannonSignal :: Int -> SignalGen(Signal CannonState)
+cannonSignal winHeight = signal
+  where initialState = CannonState {cx = 0, cy = realToFrac winHeight / 2 * 0.8}
+        signal = foldp moveCannon initialState Keyboard.arrows
+        moveCannon :: (Int, Int) -> CannonState -> CannonState
+        moveCannon (dx, _) state = state {cx = cx state + realToFrac dx}
 
-moveSpaceship :: (Int, Int) -> SpaceshipState -> SpaceshipState
-moveSpaceship (dx, _) state = state {mx = mx state + realToFrac dx}
+cannonForm :: CannonState -> Form
+cannonForm state = move (cx state, cy state) $ filled red $ square 64
 
-render :: SpaceshipState -> (Int, Int) -> Element
-render state (w, h) = centeredCollage w h [move (mx state, my state) $ filled red $ square 64]
+laserSignal :: SignalGen(Signal LaserState)
+laserSignal = signal
+  where initialState = LaserState {ly = 0, flying = False}
+        signal = foldp fireLaser initialState Keyboard.space
+        fireLaser :: Bool -> LaserState -> LaserState
+        fireLaser fired state = state {flying = flying, ly = ly'}
+          where flying = (fired && (ly state == 0)) || (ly state /= 0 && ly state < 300)
+                ly' = if flying then ly state + 1 else 0
+
+laserForm :: LaserState -> CannonState -> Form
+laserForm laserState cannonState = move (lx', ly') $ filled white $ rect 2 10
+  where lx' = cx cannonState
+        ly' = cy cannonState - 30 - ly laserState
+
+render :: CannonState -> LaserState -> (Int, Int) -> Element
+render cannonState laserState (w, h) = centeredCollage w h [cannonForm cannonState,
+                                                            laserForm laserState cannonState]
 
 main :: IO ()
 main = do
-  run config $ render <~ (createSpaceship winHeight) ~~ Window.dimensions
+  run config $ render <~ (cannonSignal winHeight) ~~ laserSignal ~~ Window.dimensions
   where config = defaultConfig {windowTitle = "space_invaders",
                                 windowDimensions = (winWidth, winHeight)}
         (winWidth, winHeight) = (500, 500)
