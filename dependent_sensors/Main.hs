550fd296865ec8b7b15889729acde1f213b51101
Alex Greif <alex@greifdesign.net>
2014-06-09 16:09:41 +0200

    refactoring

--- dependent_sensors/Main.hs
+++ dependent_sensors/Main.hs
@@ -4,41 +4,36 @@ import qualified FRP.Helm.Keyboard as Keyboard
 import qualified FRP.Helm.Text as Text
 import FRP.Elerea.Simple
 
-data State = State {mx :: Double, my :: Double, color :: Color, text :: String}
+data SquareState = SquareState {mx :: Double, my :: Double, color :: Color, text :: String}
 
-
-stepper1 :: SignalGen(Signal State)
-stepper1 = foldp step state1 Keyboard.arrows
-  where state1 = State {mx = 0, my = 0, color = red, text = "A"}
-        step :: (Int, Int) -> State -> State
+redSquare :: SignalGen(Signal SquareState)
+redSquare = foldp step initialState Keyboard.arrows
+  where initialState = SquareState {mx = 0, my = 0, color = red, text = "A"}
+        step :: (Int, Int) -> SquareState -> SquareState
         step (dx, dy) state = state {mx = mx state + realToFrac dx,
                                       my = my state + realToFrac dy}
 
+blueSquare :: SignalGen(Signal SquareState)
+blueSquare = foldp step initialState signal
+  where initialState = SquareState {mx = 0, my = 100, color = blue, text = "B"}
+        signal = lift2 (\(dx, dy) stateRed -> ((dx, dy), stateRed)) Keyboard.wasd redSquare
+        step :: ((Int, Int), SquareState) -> SquareState -> SquareState
+        step ((dx, dy), stateRed) stateBlue = stateBlue {mx = mx stateBlue + realToFrac dx,
+                                                       my = my stateBlue + realToFrac dy,
+                                                       text = show distance}
+          where distance = round $ sqrt ((mx stateRed - mx stateBlue)^2
+                                         + (my stateRed - my stateBlue)^2)
 
-
-stepper2 :: SignalGen(Signal State)
-stepper2 = foldp step state2 signal
-  where state2 = State {mx = 0, my = 100, color = blue, text = "B"}
-        signal = lift2 combine' Keyboard.wasd stepper1
-        combine' :: (Int, Int) -> State -> ((Int, Int), State)
-        combine' (dx, dy) state1 = ((dx, dy), state1)
-        step :: ((Int, Int), State) -> State -> State
-        step ((dx, dy), state1) state2 = state2 {mx = mx state2 + realToFrac dx,
-                                                 my = my state2 + realToFrac dy,
-                                                 text = show distance}
-          where distance = round $ sqrt ((mx state1 - mx state2)^2 + (my state1 - my state2)^2)
-
-
-form :: State -> [Form]
+form :: SquareState -> [Form]
 form state = [move (mx state, my state) $ filled (color state) $ square 64,
               move (mx state, my state) $ toForm $ Text.plainText $ text state]
 
-render :: State -> State -> (Int, Int) -> Element
-render state1 state2 (w, h) =
-  centeredCollage w h $ form state1 ++ form state2
+render :: SquareState -> SquareState -> (Int, Int) -> Element
+render stateRed stateBlue (w, h) =
+  centeredCollage w h $ form stateRed ++ form stateBlue
 
 main :: IO ()
-main = run config $ render <~ stepper1 ~~ stepper2 ~~ Window.dimensions
+main = run config $ render <~ redSquare ~~ blueSquare ~~ Window.dimensions
   where config = defaultConfig {windowTitle = "dependent_sensors",
                                 windowDimensions = (500, 500)}
 
